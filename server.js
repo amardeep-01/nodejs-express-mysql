147dc3cd25f6e7bfa70c40bce11365dc6c621668
AMARDEEP KINI <92258265+Akamardeep@users.noreply.github.com>
2024-04-19 01:12:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
-process.title = "MyNodeApp";
-process.env.NODE_APP_NAME = 'MyApp';
+const appNameToFind = 'MyApp';
 //demo
 const express = require("express");
 // const bodyParser = require("body-parser"); /* deprecated */
